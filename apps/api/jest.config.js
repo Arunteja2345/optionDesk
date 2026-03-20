231693c50badbaf20c80abfe61800c5080df5251
Arunteja2345 <aruntejapulaparthi9@gmail.com>
2026-03-20 11:52:51 +0530

    fix: exclude tests from prod build, separate test tsconfig

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: './tsconfig.test.json',
+    }]
+  },
   moduleNameMapper: {
     '^@shared/(.*)$': '<rootDir>/src/$1',
   },
